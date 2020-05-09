35245684db1caec9e51bce06b8080ede064bd530
unknown <damakuno@gmail.com>
2020-05-09 16:19:58 +0800

    merge commit

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require("./models/model")
+const PORT = process.env.PORT || 3000;
+const fs = require("fs")
 const app = express();
 
+
+
+var countries = fs.readFileSync('./public/text/countries.txt', 'utf-8')
+countries = countries.split('\r\n')
 app.set('views', './views')
 app.set('view engine', 'pug')
 
@@ -10,33 +17,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 app.use((req, res, next) => {
-   console.log(req.method + "----->" + req.path +"----->" + req.ip);
-   next();  
+    console.log("Method: " + req.method + " -----> " + "Path: " + req.path + " -----> " + "Requested IP: " + req.ip);
+    next();
 });
 
-<<<<<<< HEAD
-app.get('/', (req, res) => {
-	res.send(`<a href="/pug">hello pug</a><br />
-  <a href="/test">hello tet</a>`);
-});
-=======
-app.get("/", function(req,res){
-    res.render("index.pug",{"title":"Lahoot-Home"})
-})
->>>>>>> b200880a2c780ea9c10fb666aae0664f0a6e3d72
-
-app.get("/games", function(req,res){
-    res.render("index.pug",{"title":"Lahoot-Games"})
+app.get("/", function (req, res) {
+    res.render("home.pug", { "title": "Lahoot-Home" })
 })
 
-app.get("/register", function(req,res){
-    res.render("index.pug",{"title":"Lahoot-Register"})
+app.get("/games", function (req, res) {
+    res.render("games.pug", { "title": "Lahoot-Games" })
 })
 
-app.post("/login", function(req,res){
-    res.render("index.pug",{"title":"Lahoot-Login"})
+app.get("/register", function (req, res) {
+    res.render("register.pug", { "title": "Lahoot-Register", "countries": countries })
 })
 
-app.listen(3000 , function(){
-    console.log("The Server is Started")
+app.post("/login", function (req, res) {
+    res.render("login.pug", { "title": "Lahoot-Login" })
+})
+
+app.listen(PORT, function () {
+    console.log(`The Server is Started @ ${PORT}`)
 });
--- readme.md
+++ readme.md
@@ -2,3 +2,31 @@
 This is a Kahoot.it clone
 
 Run `npm install` to install related dependencies
+
+# [App](app/)
+
+This folder contains the related app features of the website
+
+# [Models](models/)
+
+This folder contains the Database models related to the website
+
+# [Public CSS](public/css/)
+
+This folder contains the CSS files related to the website
+
+# [Public JS](public/js/)
+
+This folder contains the JS files related to the website
+
+# [Public Images](public/images/)
+
+This folder contains the image files related to the website
+
+# [Public Texts](public/text/)
+
+This folder contains the text files related to the website
+
+# [Views](views/)
+
+This folder contains the Views pug files related to the website
