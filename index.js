f1af5241d6b7828c3196c8f45423866f4479e5e2
SRINIVASAN CHAKARAPANI <chakarai1234@gmail.com>
2020-05-10 13:50:18 +0800

    Merge Conflicts

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const model = require("./models/model.js")
-const wiz = require('./app/app.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const model = require("./models/model.js");
+const wiz = require("./app/app.js");
 const PORT = process.env.PORT || 3000;
 const fs = require("fs");
 const app = express();
 
-var countries = fs.readFileSync('./public/text/countries.txt', 'utf-8')
-countries = countries.split('\r\n')
+var countries = fs.readFileSync("./public/text/countries.txt", "utf-8");
+countries = countries.split("\r\n");
 
 model.init();
 // let qq = new wiz.QuizQuestion();
@@ -19,50 +19,74 @@ model.init();
 //     console.dir(qs);
 // }).catch(err => console.log(err));
 
-
-
-app.set('views', './views')
-app.set('view engine', 'pug')
+app.set("views", "./views");
+app.set("view engine", "pug");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use((req, res, next) => {
-    console.log("Method: " + req.method + " -----> " + "Path: " + req.path + " -----> " + "Requested IP: " + req.ip);
-    next();
+  console.log(
+    "Method: " +
+    req.method +
+    " -----> " +
+    "Path: " +
+    req.path +
+    " -----> " +
+    "Requested IP: " +
+    req.ip
+  );
+  next();
 });
 
 app.get("/", function (req, res) {
-    res.render("home.pug", { "title": "Lahoot-Home" })
-})
+  res.render("home.pug", {
+    title: "Lahoot-Home",
+  });
+});
 
 app.get("/games", function (req, res) {
-    console.log(req.params);
-    res.render("games.pug", { "title": "Lahoot-Games" })
-})
+  console.log(req.params);
+  res.render("games.pug", {
+    title: "Lahoot-Games",
+  });
+});
 
 app.get("/games/:gameId", function (req, res) {
-    // console.log(req.params);
-    // let game = new wiz.Game();
-    // game.load(req.params.gameId).then((game) => {
-    res.render("games.pug", { "title": "Lahoot-Games" });
-    // }
-    // ).catch(err => { })
-})
+  // console.log(req.params);
+  // let game = new wiz.Game();
+  // game.load(req.params.gameId).then((game) => {
+  res.render("games.pug", {
+    title: "Lahoot-Games",
+  });
+  // }
+  // ).catch(err => { })
+});
 
 app.get("/register", function (req, res) {
-    res.render("register.pug", { "title": "Lahoot-Register", "countries": countries })
-})
+  res.render("register.pug", {
+    title: "Lahoot-Register",
+    countries: countries,
+  });
+});
 
 app.get("/login", function (req, res) {
-    res.render("login.pug", { "title": "Lahoot-Login" })
-})
+  res.render("login.pug", {
+    title: "Lahoot-Login",
+  });
+});
 
 app.post("/login", function (req, res) {
-    res.render("login.pug", { "title": "Lahoot-Login" })
-})
+  res.render("login.pug", {
+    title: "Lahoot-Login",
+  });
+});
 
 app.listen(PORT, function () {
-    console.log(`The Server is Started @ ${PORT}`)
+  console.log(`The Server is Started @ ${PORT}`);
 });
